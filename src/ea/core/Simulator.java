676d5a850068f17afd2d2df73e6bf56cde940e9c
kyrrelm <kyrre.l.moe@gmail.com>
2016-02-24 16:27:13 +0100

    sigma scaling

--- src/ea/core/Simulator.java
+++ src/ea/core/Simulator.java
@@ -15,9 +15,11 @@ public class Simulator {
     private static List<Individual> childPopulation;
     private static List<Individual> adultPopulation;
 
-    public static int populationSize = 100;
+    public static int populationSize = 1000;
     public static int productionSize = populationSize;
-    public static float crossoverRate = 0.2f;
+    //0.2 Best for OneMax: 1f
+    public static float crossoverRate = 0.5f;
+    //0.01 Best for OneMax: 0f
     public static float perComponentMutationRate = 0.01f;
     public static float elitism = 0.1f;
     public static AdultSelection adultSelection = OVER_PRODUCED_GENERATIONAL_MIXING;
@@ -63,13 +65,19 @@ public class Simulator {
         for (Individual i: adultPopulation) {
             totalFitness += i.getFitness();
         }
+        double averageFitness = (double) totalFitness/adultPopulation.size();
+        double standardSum = 0;
+        for (Individual i: adultPopulation) {
+            standardSum+=Math.pow((i.getFitness()-averageFitness),2);
+        }
+        double standardDeviation = Math.sqrt(standardSum/adultPopulation.size());
 
         for (int i = 0; i < productionSize /2; i++) {
-            Individual firstPick = spinWheel(totalFitness);
+            Individual firstPick = spinWheel(totalFitness, averageFitness, standardDeviation);
             Individual secondPick = null;
             boolean duplicate = true;
             while (duplicate){
-                secondPick = spinWheel(totalFitness);
+                secondPick = spinWheel(totalFitness, averageFitness, standardDeviation);
                 duplicate = secondPick == firstPick;
             }
             mate(firstPick, secondPick);
@@ -78,15 +86,24 @@ public class Simulator {
 
     }
 
-    private static void mate(Individual firstPick, Individual secondPick) {
-        childPopulation.add(new Individual(firstPick.genotype.crossover(secondPick.genotype)));
-        if (childPopulation.size()< productionSize){
-            childPopulation.add(new Individual(secondPick.genotype.crossover(firstPick.genotype)));
-
-        }
+    private static Individual spinWheel(int totalFitness, double averageFitness, double standardDeviation) {
+        //return fitnessProportionate(totalFitness);
+        return sigma(averageFitness,standardDeviation);
     }
 
-    private static Individual spinWheel(int totalFitness){
+    private static Individual sigma(double averageFitness, double standardDeviation){
+        double partialSum = 0;
+        double rand = new Random().nextDouble()*adultPopulation.size();
+        for (Individual i: adultPopulation) {
+            partialSum += 1+((i.getFitness()-averageFitness)/(2*standardDeviation));
+            if (partialSum > rand){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    private static Individual fitnessProportionate(int totalFitness){
         int partialSum = 0;
         int rand = new Random().nextInt(totalFitness);
         for (Individual i: adultPopulation) {
@@ -96,6 +113,14 @@ public class Simulator {
             }
         }
         return null;
+    }
+
+    private static void mate(Individual firstPick, Individual secondPick) {
+        childPopulation.add(new Individual(firstPick.genotype.crossover(secondPick.genotype)));
+        if (childPopulation.size()< productionSize){
+            childPopulation.add(new Individual(secondPick.genotype.crossover(firstPick.genotype)));
+
+        }
     }
 
     private static void adultSelection() {
