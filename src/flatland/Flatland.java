427b139096f2def4f01c43ea0419066c5b6b9121
kyrre <kyrre.l.moe@gmail.com>
2016-04-07 16:26:36 +0200

    cleanup

--- src/flatland/Flatland.java
+++ src/flatland/Flatland.java
@@ -2,16 +2,8 @@ package flatland;/**
  * Created by Kyrre on 06.04.2016.
  */
 
-import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
@@ -23,17 +15,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Flatland extends Application {
 
     Player player = Player.O_PLAYER;
     Button[] cells;
-    double playbackSpeed = 500;
-    final double MAX_PLAYBACK_INTERVAL = 5000;
+    double playbackInterval = 3000;
+    final double MAX_PLAYBACK_INTERVAL = 3000;
     final double MIN_PLAYBACK_INTERVAL = 100;
     long sinceLast = 0;
 
@@ -57,13 +45,14 @@ public class Flatland extends Application {
                 board.add(cells[num++], i, j);
             }
         }
+        //GUI setup
         Label labelSpeedSlider = new Label("Animation speed:");
-        Slider speedSlider = new Slider(100, MAX_PLAYBACK_INTERVAL, playbackSpeed);
+        Slider speedSlider = new Slider(100, MAX_PLAYBACK_INTERVAL, playbackInterval);
         speedSlider.setOrientation(Orientation.HORIZONTAL);
         speedSlider.setMaxWidth(300);
         speedSlider.setPrefWidth(300);
-        speedSlider.setBlockIncrement(100);
-        speedSlider.setValue(playbackSpeed);
+        speedSlider.setBlockIncrement(1000);
+        speedSlider.setValue(MAX_PLAYBACK_INTERVAL- playbackInterval +MIN_PLAYBACK_INTERVAL);
 
         HBox hbox = new HBox();
         hbox.setPadding(new Insets(5, 5, 5, 5));
@@ -71,79 +60,28 @@ public class Flatland extends Application {
         hbox.getChildren().addAll(labelSpeedSlider,speedSlider);
         board.add(hbox,0,10,10,1);
 
-
-
         Scene scene = new Scene(board);
         primaryStage.setScene(scene);
         primaryStage.setTitle("TicTacToe By Legato");
         primaryStage.getIcons().add(new Image("flatland/img/icons.png", 50, 50, false, false));
         primaryStage.show();
 
-        Timeline timer = new Timeline();
-        timer.setCycleCount(Timeline.INDEFINITE);
-
+        //GUI updates
         new AnimationTimer() {
             @Override
             public void handle(long now) {
-                if (now - sinceLast > playbackSpeed*1000000){
+                if (now - sinceLast > playbackInterval *1000000){
                     sinceLast = now;
-                    System.out.println("happend");
+                    setButton((int) (Math.random()*100));
                 }
             }
         }.start();
 
-        KeyFrame keyFrame = new KeyFrame(
-                Duration.seconds(1000),
-                //ae -> setButton((int) (Math.random()*100)));
-                ae -> System.out.println("hur dur"));
-        timer.stop();
-        timer.getKeyFrames().setAll(
-                keyFrame
-        );
-        timer.play();
-
         speedSlider.valueProperty().addListener(cl -> {
-            playbackSpeed = speedSlider.getValue();
-            System.out.println("speed slider: "+playbackSpeed);
-            //resetTimer(timer, playbackSpeed);
+            playbackInterval = MAX_PLAYBACK_INTERVAL-speedSlider.getValue()+MIN_PLAYBACK_INTERVAL;
+            System.out.println("speed slider: "+ playbackInterval);
+            //resetTimer(timer, playbackInterval);
         });
-
-        //resetTimer(timer, playbackSpeed);
-
-
-
-
-//        Timeline timeline = new Timeline(new KeyFrame(
-//                Duration.millis(playbackSpeed),
-//                ae -> setButton((int) (Math.random()*100))));
-//        timeline.setCycleCount(Animation.INDEFINITE);
-//        timeline.play();
-
-
-//        int i = 0;
-//        while (true)
-//        {
-//            final int finalI = i++;
-//            Platform.runLater ( () -> label.setText ("" + finalI));
-//            Thread.sleep (1000);
-//        }
-    }
-
-    private void resetTimer(Timeline timer, final double timerInterval) {
-//        timer = new Timeline(new KeyFrame(
-//        Duration.millis(playbackSpeed),
-//        ae -> setButton((int) (Math.random()*100))));
-//        timer.setCycleCount(Animation.INDEFINITE);
-//        timer.play();
-        KeyFrame keyFrame = new KeyFrame(
-                Duration.seconds(timerInterval),
-                ae -> setButton((int) (Math.random()*100)));
-                //ae -> System.out.println("hur dur"));
-        timer.stop();
-        timer.getKeyFrames().setAll(
-                keyFrame
-        );
-        timer.play();
     }
 
     private void setButton(int index){
