9a4cbcfb8f2b14113a2dcaaeef7a3c9ac1c8b1ce
kyrre <kyrre.l.moe@gmail.com>
2016-04-07 16:11:00 +0200

    slider works

--- src/flatland/Flatland.java
+++ src/flatland/Flatland.java
@@ -3,25 +3,39 @@ package flatland;/**
  */
 
 import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Flatland extends Application {
 
     Player player = Player.O_PLAYER;
     Button[] cells;
-    GridPane outerGrid;
+    double playbackSpeed = 500;
+    final double MAX_PLAYBACK_INTERVAL = 5000;
+    final double MIN_PLAYBACK_INTERVAL = 100;
+    long sinceLast = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,7 +45,6 @@ public class Flatland extends Application {
     public void start(Stage primaryStage) {
         Image imgBlank = new Image("flatland/img/blank.png", 50, 50, false, false);
         cells = new Button[100];
-        outerGrid = new GridPane();
         for (int i = 0; i < cells.length; i++) {
             cells[i] = new Button("", new ImageView(imgBlank));
             registerOnAction(cells[i]);
@@ -44,27 +57,68 @@ public class Flatland extends Application {
                 board.add(cells[num++], i, j);
             }
         }
-        Slider slider = new Slider(-100, 100, 0);
-        slider.setOrientation(Orientation.HORIZONTAL);
-        slider.setMaxWidth(400);
-        slider.setBlockIncrement(100);
-        //setHgrow(slider, Priority.ALWAYS);
+        Label labelSpeedSlider = new Label("Animation speed:");
+        Slider speedSlider = new Slider(100, MAX_PLAYBACK_INTERVAL, playbackSpeed);
+        speedSlider.setOrientation(Orientation.HORIZONTAL);
+        speedSlider.setMaxWidth(300);
+        speedSlider.setPrefWidth(300);
+        speedSlider.setBlockIncrement(100);
+        speedSlider.setValue(playbackSpeed);
 
-        outerGrid.add(board,0,0);
-        outerGrid.add(slider, 0, 1);
-        //outerGrid.add();
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(5, 5, 5, 5));
+        hbox.setSpacing(5);
+        hbox.getChildren().addAll(labelSpeedSlider,speedSlider);
+        board.add(hbox,0,10,10,1);
 
-        Scene scene = new Scene(outerGrid);
+
+
+        Scene scene = new Scene(board);
         primaryStage.setScene(scene);
         primaryStage.setTitle("TicTacToe By Legato");
         primaryStage.getIcons().add(new Image("flatland/img/icons.png", 50, 50, false, false));
         primaryStage.show();
 
-        Timeline timeline = new Timeline(new KeyFrame(
-                Duration.millis(2500),
-                ae -> setButton((int) (Math.random()*100))));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
+        Timeline timer = new Timeline();
+        timer.setCycleCount(Timeline.INDEFINITE);
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (now - sinceLast > playbackSpeed*1000000){
+                    sinceLast = now;
+                    System.out.println("happend");
+                }
+            }
+        }.start();
+
+        KeyFrame keyFrame = new KeyFrame(
+                Duration.seconds(1000),
+                //ae -> setButton((int) (Math.random()*100)));
+                ae -> System.out.println("hur dur"));
+        timer.stop();
+        timer.getKeyFrames().setAll(
+                keyFrame
+        );
+        timer.play();
+
+        speedSlider.valueProperty().addListener(cl -> {
+            playbackSpeed = speedSlider.getValue();
+            System.out.println("speed slider: "+playbackSpeed);
+            //resetTimer(timer, playbackSpeed);
+        });
+
+        //resetTimer(timer, playbackSpeed);
+
+
+
+
+//        Timeline timeline = new Timeline(new KeyFrame(
+//                Duration.millis(playbackSpeed),
+//                ae -> setButton((int) (Math.random()*100))));
+//        timeline.setCycleCount(Animation.INDEFINITE);
+//        timeline.play();
+
 
 //        int i = 0;
 //        while (true)
@@ -75,8 +129,25 @@ public class Flatland extends Application {
 //        }
     }
 
+    private void resetTimer(Timeline timer, final double timerInterval) {
+//        timer = new Timeline(new KeyFrame(
+//        Duration.millis(playbackSpeed),
+//        ae -> setButton((int) (Math.random()*100))));
+//        timer.setCycleCount(Animation.INDEFINITE);
+//        timer.play();
+        KeyFrame keyFrame = new KeyFrame(
+                Duration.seconds(timerInterval),
+                ae -> setButton((int) (Math.random()*100)));
+                //ae -> System.out.println("hur dur"));
+        timer.stop();
+        timer.getKeyFrames().setAll(
+                keyFrame
+        );
+        timer.play();
+    }
+
     private void setButton(int index){
-        System.out.println("setButton");
+        //System.out.println("setButton");
         cells[index].setGraphic(new ImageView(retrieveMarker()));
     }
 
