b4666d42b2774a1eb9860e8e1e49b1176f6cfb74
kyrre <kyrre.l.moe@gmail.com>
2016-04-12 14:02:27 +0200

    network seems to work

--- src/ann/Network.java
+++ src/ann/Network.java
@@ -1,6 +1,7 @@
 package ann;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Kyrre on 11.04.2016.
@@ -11,19 +12,22 @@ public class Network {
     private ArrayList<ArrayList<Neuron>> hiddenLayers;
     private ArrayList<Neuron> outputNodes;
 
-    public Network(int numberOfInputNodes, int numberOfOutputNodes, int... hiddenNodesInLayer) {
+    public Network(int numberOfInputNodes, int numberOfOutputNodes, float[] weights, int... hiddenNodesInLayer) {
         this.inputNodes = new ArrayList<>();
         this.hiddenLayers = new ArrayList<>();
         this.outputNodes = new ArrayList<>();
         for (int i = 0; i < numberOfInputNodes; i++) {
             inputNodes.add(new Neuron(0.5f));
         }
+        int weightIndex = 0;
         ArrayList<Neuron> previousLayer = inputNodes;
         for (int hiddenNodes: hiddenNodesInLayer) {
             ArrayList<Neuron> layer = new ArrayList<>();
             for (int i = 0; i < hiddenNodes; i++) {
                 Neuron n = new Neuron(0.5f);
-                //n.connect(previousLayer);
+                for (Neuron parent : previousLayer) {
+                    n.connect(new Connection(parent, weights[weightIndex++]));
+                }
                 layer.add(n);
             }
             hiddenLayers.add(layer);
@@ -31,25 +35,36 @@ public class Network {
         }
         for (int i = 0; i <numberOfOutputNodes; i++) {
             Neuron outputNode = new Neuron(0.5f);
-            //outputNode.connect(previousLayer);
+            for (Neuron parent:previousLayer) {
+                outputNode.connect(new Connection(parent,weights[weightIndex++]));
+            }
             outputNodes.add(outputNode);
         }
     }
 
-    public int[] run(float... inputs){
+    public float[] run(float... inputs){
         for (int i = 0; i <inputs.length; i++) {
             inputNodes.get(i).setValue(inputs[i]);
         }
-        int[] result = new int[outputNodes.size()];
+        float[] result = new float[outputNodes.size()];
         for (int i = 0; i < result.length; i++) {
-            //outputNodes.get(i).activate();
-            result[i] = outputNodes.get(i).hasFired() ? 1 : 0;
+            outputNodes.get(i).activate(Neuron.Function.STEP);
+            result[i] = outputNodes.get(i).getValue();
         }
         return result;
     }
 
     public static void main(String[] args) {
-        Network net = new Network(2,2,2,3);
-
+        float[] weights = new float[18];
+        Random r = new Random();
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = r.nextFloat();
+        }
+        Network net = new Network(2,2,weights,2,3);
+        float[] result = net.run(1f,0.5f);
+        System.out.print("Result: ");
+        for (float f:result) {
+            System.out.print(f+", ");
+        }
     }
 }
